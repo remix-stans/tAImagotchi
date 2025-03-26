a0da89ae82a34315e2183ff69adba1d07a7b4f1a
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2025-03-25 20:13:46 -0700

    add db and auth to app load context

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -1,11 +1,14 @@
-import { DurableObject } from "cloudflare:workers";
-import { type ServerBuild, createRequestHandler } from "react-router";
-
-// @ts-expect-error - no types
-import * as build from "virtual:react-router/server-build";
-import { type Context, Hono } from "hono";
+import { betterAuth } from "better-auth";
+import { Hono } from "hono";
 import { agentsMiddleware } from "hono-agents";
 import { createMiddleware } from "hono/factory";
+import { D1Dialect } from "kysely-d1";
+import { Kysely } from "kysely";
+import type { DB } from "kysely-codegen";
+import { type ServerBuild, createRequestHandler } from "react-router";
+// @ts-expect-error - no types
+import * as build from "virtual:react-router/server-build";
+
 import { adapterContext } from "./lib/adapter-context";
 import { Tamagochi } from "./taimogochi";
 
@@ -20,6 +23,8 @@ declare global {
 
 declare module "react-router" {
 	export interface AppLoadContext {
+		db: Kysely<DB>;
+		auth: ReturnType<typeof betterAuth>;
 		cloudflare: {
 			env: CloudflareEnvironment;
 			ctx: ExecutionContext;
@@ -36,10 +41,30 @@ const reactRouter = ({ build, mode }: ReactRouterMiddlewareOptions) => {
 	return createMiddleware(async (c) => {
 		const requestHandler = createRequestHandler(build, mode);
 
+		const db = new Kysely<DB>({
+			dialect: new D1Dialect({
+				database: c.env.DB,
+			}),
+		});
+
 		const context = new Map([
 			[
 				adapterContext,
 				{
+					db,
+					auth: betterAuth({
+						database: {
+							db,
+							type: "sqlite",
+						},
+						emailAndPassword: {
+							enabled: true,
+							async sendResetPassword(data, request) {
+								// Send an email to the user with a link to reset their password
+							},
+						},
+						socialProviders: {},
+					}),
 					cloudflare: { env: c.env, ctx: c.executionCtx, cf: c.req.raw.cf },
 				},
 			],
